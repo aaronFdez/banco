51a90f9d2c80db855d0e3c265be221176d072d86
aaronFdez <aaronfdezgarcia@gmail.com>
2017-12-10 10:30:18 +0100

    cambio el correo de forma manual en vez de cogido por parametro en sitecontroller

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -103,7 +103,7 @@ class SiteController extends Controller
     public function actionContact()
     {
         $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['smtpUsername'])) {
+        if ($model->load(Yii::$app->request->post()) && $model->contact('aaronfdezgarcia@gmail.com') {
             Yii::$app->session->setFlash('contactFormSubmitted');
 
             return $this->refresh();
